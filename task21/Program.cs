052dcd6f481bd1f1611375b5754201341de09d88
Dmitriy <Dipan187@live.ru>
2023-03-14 16:14:00 +0300

    Задача 21

--- task21/Program.cs
+++ task21/Program.cs
@@ -3,3 +3,19 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
+
+Console.Clear();
+Console.Write("Введите точку x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите точку y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите точку z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите точку x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите точку y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите точку z2: ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2)));
