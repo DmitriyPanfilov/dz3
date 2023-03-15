d5840803bc9912f9536451935cfff818ae046dd1
Dmitriy <Dipan187@live.ru>
2023-03-15 16:53:27 +0300

    Задача 19

--- task19/Program.cs
+++ task19/Program.cs
@@ -4,3 +4,33 @@
 // 12821 -> да
 // 23432 -> да
 
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int remineder, sum = 0;
+        int temp = number;
+        while (number > 0)
+        {
+            remineder = number % 10; //разделяем число по 1 цифре
+          
+            sum = (sum * 10) + remineder; //переворачиваем число прибавляя к последней цфре
+        
+            number = number / 10; //проверяем количество оставшихся цифр в числе
+           
+        }
+        if (temp == sum)
+        {
+            Console.WriteLine($"Number {temp} is Palindrome.");
+        }
+        else
+        {
+            Console.WriteLine($"Number {temp} is not Palindrome");
+        }
+        Console.ReadKey();
+    }
+}
+
